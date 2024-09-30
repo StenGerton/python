fff03a62294f2949a667abe3fa895d1c361ea220
StenGerton <stengerton@hkhk.edu.ee>
2024-09-30 11:44:06 +0300

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,24 @@
+# Sten-Gerton Tiitmaa
+# Harjutus 01
+# 30.09.24
+#string - tekst, int - täisarv ,float - komaarv
+
+
+print('tere\n\"SUUR\" maailm!')
+print('Tere sulle ka!')
+print("\"Cabernet Sauvignon\" või 'Cabernet Sauvignon'?")
+print("  @..@")
+print(" (----) ")
+print("  \__/ ")
+print('^^ "" ^^')
+a = 500
+b = a + 100
+nimi = "Jüri"
+
+a = int (input("sisesta ruudu külje pikkus: "))
+ringiP = 2*3.14*r
+#print(type(r))
+ruuduS= a * a
+print(ringiP)
+#print(nimi,"on",b,"aastane")
+print("Vastus: ruudu pindala on",ruuduS,"cm^2")
---
+++ 02.py
@@ -0,0 +1,45 @@
+
+#Sen-Gerton Tiitmaa
+#30.09.24
+#harjutus 02
+
+
+
+
+#hüpotenuus **
+ a,b = 16,9
+c = sqrt(a)
+print("Hüpotenuus on:",c)
+ruutjuur = (arv)
+
+
+
+
+
+#kütusekulu arvutamine
+
+#Ajateisendus // %
+aeg = int(input("Lisa aeg minutites: "))
+aeg = 72
+h = aeg/60
+m = aeg % 60
+print(h,":",m)
+
+
+
+
+
+
+
+
+
+l = int(input("Lisa tangitud liitrid: "))
+km = int(input("Lisa läbitud kilomeetrid: "))
+kytusekulu = l/(km/100)
+print("Sinu kütusekulu",kytusekulu)
+
+#arvusüsteemid
+arv = int(input("Lisa arv: "))
+kahend = bin(arv)
+kuust = hex(arv)
+print("Sinu teisendused on: ",kahend, "ja",kuust)
