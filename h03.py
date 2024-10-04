ee910de019c0a9633820eb2b2f44ba0e931ef14e
StenGerton <stengerton@hkhk.edu.ee>
2024-10-04 14:01:00 +0300

    Add files via upload

---
+++ h03.py
@@ -0,0 +1,16 @@
+import turtle
+#Sten-Gerton Tiitmaa
+#04.10.24
+turtle.penup()
+turtle.goto(-500,300)
+turtle.pendown()
+#kolmnurk
+turtle.fd(200)
+turtle.left(120)
+turtle.fd(200)
+turtle.left(120)
+turtle.fd(200)
+turtle.left(120)
+
+#lõpetan et ei jookseks kokku
+turtle.done()
