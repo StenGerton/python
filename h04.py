e59204aa102b35b3febc48a8155fb25cc7ad8233
StenGerton <stengerton@hkhk.edu.ee>
2024-10-07 14:28:45 +0300

    Add files via upload

---
+++ h04.py
@@ -0,0 +1,15 @@
+#stengerton
+#07.10.24
+
+t = "Hello World"
+print(t[len(t)-1])
+
+tekst = "Hello World"
+print(tekst.capitalize())
+print(tekst.upper())
+print(tekst.lower())
+print(tekst.swapcase())
+
+tekst = "           Hello World                "
+print(tekst.strip())
+print(tekst.rstrip())
