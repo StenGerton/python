7cebda75fe40c246cc1ef9b0609dc1265a3bac33
StenGerton <stengerton@hkhk.edu.ee>
2024-12-12 09:57:32 +0200

    .

---
+++ 001.py
@@ -0,0 +1,48 @@
+
+
+#sook iga osaleja peale 10 eurot
+#ruum 55 eurot
+#küsi mitu inimest on kutsutud
+#küsi mitu inimest tuleb
+#prindi maksimum eelarvet
+#prindi minimaalset eelarvet
+
+
+
+
+
+# inimesed = int(input("Mitu inimest on kutsutud: "))
+# tulemas = int(input("Min inimest tuleb?: "))
+# eelarve = tulemas * 10 + 55
+# print(eelarve)
+# max = inimesed * 10 + 55
+# print(max)
+# min = tulemas * 10 + 55
+# print(min)
+
+
+
+
+
+
+
+
+külaliste_arv =int(input("Sisestage külaliste arv:"))
+nimi = "võõrustaja:"
+print(nimi, "Tere!")
+nimi1 = "külaline:"
+print(nimi1, "Tere, suur tänu kutse eest!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test.py
@@ -0,0 +1,83 @@
+
+
+
+
+
+
+
+def mahlapakkide_arv(ountekogus):
+    mahlapakkidearv = int(round(ountekogus * 0.4/3,0))
+    return mahlapakkidearv
+kg = float(input("õunte kogus: "))
+print(mahlapakkide_arv(kg))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#4.1 banner
+
+
+
+
+# mitu = int(input("Mitu korda: "))
+# lause = input("Sinu reklaamlause:")
+
+
+# def banner(l):
+#     print(l.upper)
+
+
+# for i in range(mitu):
+#     banner(lause)
+
+
+
+
