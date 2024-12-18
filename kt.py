9557c463851b73baf144e67c7a6090383e98025b
StenGerton <stengerton@hkhk.edu.ee>
2024-12-18 13:58:54 +0200

    Create kt.py

---
+++ kt.py
@@ -0,0 +1,65 @@
+# #9
+# gmail = input("lisa gmail:")
+
+# if '@' in gmail:
+#     kasutaja = 
+#     server = 
+#     print(f"Tere {kasutaja}, sinu email on {server} serveris ja domeeniks on sul .com")
+# else:
+#     print("Sisestasite valesti gmaili")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+# #6
+# number = int(input("Sisesta arv: "))
+
+# if number % 2 == 0:
+#     print(f"Arv {number} on paaris.")
+# else:
+#     print(f"Arv {number} on paaritu.")
+
+
+# 3 
+positiivsed = []
+negatiivsed = []
+number = int(input("Sisesta arv: "))
+for i in range(5):
+    if number > 0:
+        positiivsed += 1
+    elif number < 0:
+        negatiivsed += 1
+    else:
+        print("Sisestasite nulli")
+
+
+
+
+
+
+
+
+
+#4 
+
+
+
+
+
+
+
+
+
+
+
+
+# #13
