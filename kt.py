37e909452ace595b86be3ce6a1d8e57b65847878
StenGerton <stengerton@hkhk.edu.ee>
2024-12-19 13:47:00 +0200

    Update kt.py

--- kt.py
+++ kt.py
@@ -1,10 +1,10 @@
-# #9
+#Sten-Gerton Tiitmaa 18.12.24 ül 9 6 3 4 13
+
+#9
 # gmail = input("lisa gmail:")
 
-# if '@' in gmail:
-#     kasutaja = 
-#     server = 
-#     print(f"Tere {kasutaja}, sinu email on {server} serveris ja domeeniks on sul .com")
+# if '@' in gmail:        
+#     print("Tere, " + str(gmail.split("@")[0].split(".")[0].capitalize()) +","+" sinu email on server serveris ja domeeniks on sul com")
 # else:
 #     print("Sisestasite valesti gmaili")
     
@@ -23,23 +23,22 @@
 # #6
 # number = int(input("Sisesta arv: "))
 
-# if number % 2 == 0:
+# if number == 0:
+#     print("Sisestasite nulli.")
+# elif number % 2 == 0:
 #     print(f"Arv {number} on paaris.")
 # else:
 #     print(f"Arv {number} on paaritu.")
 
 
 # 3 
-positiivsed = []
-negatiivsed = []
-number = int(input("Sisesta arv: "))
-for i in range(5):
-    if number > 0:
-        positiivsed += 1
-    elif number < 0:
-        negatiivsed += 1
-    else:
-        print("Sisestasite nulli")
+# positiivsed = 
+# negatiivsed = 
+# number = int(input("Sisesta arv: "))
+# for i in range(5):
+#     if number
+
+
 
 
 
@@ -50,8 +49,7 @@ for i in range(5):
 
 
 #4 
-
-
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]; x = [i for i in a if i < 5]; print(x)
 
 
 
@@ -63,3 +61,11 @@ for i in range(5):
 
 
 # #13
+# number = int(input("Sisesta arv: "))
+
+# if number == 0:
+#     print("Sisestasite nulli.")
+# elif number % 2 == 0:
+#     print(f"Arv {number} on paaris.")
+# else:
+#     print(f"Arv {number} on paaritu.")
